3c644fd2e58c7290f3fcafc5d47af9bf964ee934
duyuefeng0708 <duyuefeng0708@gmail.com>
2018-09-06 22:35:35 +0800

    search fileid1

--- private/DSSE/StoreIndex.js
+++ private/DSSE/StoreIndex.js
@@ -22,7 +22,7 @@ var total;
 
 function read_json(){
 
-index = require("./buildIndex/db/labelfinance200.json");
+index = require("./buildIndex/db/labelfinance255.json");
 //index = require("./local/db/labeltest2.json");
 //string = require("./local/db/randomstring.json");
 
